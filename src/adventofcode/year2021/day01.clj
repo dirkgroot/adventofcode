3d41d194ed6d8c09060bb954fcff716a2119384a
Dirk Groot <dirk.inge@gmail.com>
2021-11-14 17:18:20 +0100

    Add placeholder for day 1

---
+++ src/adventofcode/year2021/day01.clj
@@ -0,0 +1,16 @@
+(ns adventofcode.year2021.day01)
+
+(defn parse-input [input]
+  input)
+
+(defn part1 [input] input)
+(defn part2 [input] input)
+
+(def puzzle
+  {:year        2021
+   :day         1
+   :parse-input parse-input
+   :part1       part1
+   :part2       part2
+   :answer1     nil
+   :answer2     nil})
---
+++ test/adventofcode/year2021/solutions.clj
@@ -0,0 +1,6 @@
+(ns adventofcode.year2021.solutions
+  (:require [clojure.test :refer :all]
+            [adventofcode.support.test :as test]
+            [adventofcode.year2021.day01 :as day01]))
+
+(deftest day01 (test/test-puzzle day01/puzzle))
