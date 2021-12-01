1633b009d0367565edb9cc6264a02730c45a5c98
Dirk Groot <d.groot@avisi.nl>
2021-12-01 07:23:04 +0100

    Day 1

--- src/adventofcode/year2021/day01.clj
+++ src/adventofcode/year2021/day01.clj
@@ -1,10 +1,17 @@
-(ns adventofcode.year2021.day01)
+(ns adventofcode.year2021.day01
+  (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  input)
+  (map #(Integer/parseInt %) (str/split-lines input)))
 
-(defn part1 [input] input)
-(defn part2 [input] input)
+(defn part1 [input]
+  (count (filter (fn [[x y]] (> y x))
+                 (partition 2 1 input))))
+
+(defn part2 [input] input
+  (count (filter (fn [[x y]] (> y x))
+                 (partition 2 1
+                            (map #(apply + %) (partition 3 1 input))))))
 
 (def puzzle
   {:year        2021
@@ -12,5 +19,5 @@
    :parse-input parse-input
    :part1       part1
    :part2       part2
-   :answer1     nil
-   :answer2     nil})
+   :answer1     1711
+   :answer2     1743})
---
+++ test/adventofcode/year2021/day01_test.clj
@@ -0,0 +1,22 @@
+(ns adventofcode.year2021.day01-test
+  (:require [clojure.test :refer :all]
+            [adventofcode.year2021.day01 :as day01]))
+
+(def example-input
+  (day01/parse-input
+    "199
+200
+208
+210
+200
+207
+240
+269
+260
+263"))
+
+(deftest part1-example
+  (is (= 7 (day01/part1 example-input))))
+
+(deftest part2-example
+  (is (= 5 (day01/part2 example-input))))
