2781aa3c4c14d50c5553c85a5c6bcefe0b24a562
Dirk Groot <d.groot@avisi.nl>
2021-12-02 17:38:16 +0100

    Day02 - improvement

--- src/adventofcode/year2021/day02.clj
+++ src/adventofcode/year2021/day02.clj
@@ -5,23 +5,19 @@
   (vec (map (fn [[cmd units]] [cmd (Integer/parseInt units)])
             (partition 2 (str/split input #"\s")))))
 
-(defn do-command-part1 [[hpos depth] [cmd units]]
-  (case cmd
-    "forward" [(+ hpos units) depth]
-    "down" [hpos (+ depth units)]
-    "up" [hpos (- depth units)]))
-
-(defn part1 [input]
-  (apply * (reduce do-command-part1 [0 0] input)))
-
-(defn do-command-part2 [[hpos depth aim] [cmd units]]
+(defn do-command [[hpos depth aim] [cmd units]]
   (case cmd
     "forward" [(+ hpos units) (+ depth (* aim units)) aim]
     "down" [hpos depth (+ aim units)]
     "up" [hpos depth (- aim units)]))
 
+(defn part1 [input]
+  (let [[hpos _ aim] (reduce do-command [0 0 0] input)]
+    (* hpos aim)))
+
 (defn part2 [input]
-  (apply * (take 2 (reduce do-command-part2 [0 0 0] input))))
+  (let [[hpos depth _] (reduce do-command [0 0 0] input)]
+    (* hpos depth)))
 
 (def puzzle
   {:year        2021
