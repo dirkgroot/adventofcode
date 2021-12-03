9b59c71ec5b926850e3d0ca15d4a1ea153d30871
Dirk Groot <d.groot@avisi.nl>
2021-12-03 17:30:54 +0100

    Day 3 - Slight simplification, still ugly

--- src/adventofcode/year2021/day03.clj
+++ src/adventofcode/year2021/day03.clj
@@ -10,34 +10,27 @@
   (reduce #(+ (* %1 2) %2) bin))
 
 (defn part1 [input]
-  (let [number-count (count input)
-        half-number-count (quot number-count 2)
+  (let [half-number-count (quot (count input) 2)
         number-of-ones (apply map + input)
         gamma (bin-to-int (map #(if (> % half-number-count) 1 0) number-of-ones))
         epsilon (bin-to-int (map #(if (< % half-number-count) 1 0) number-of-ones))]
     (* gamma epsilon)))
 
+(defn calculate-rating [input keep-requirement]
+  (loop [remaining input
+         bit 0]
+    (if (= (count remaining) 1)
+      (bin-to-int (first remaining))
+      (let [number-count (count remaining)
+            half-number-count (int (Math/ceil (/ number-count 2)))
+            number-of-ones (apply map + remaining)
+            keep-value (if (keep-requirement (nth number-of-ones bit) half-number-count) 1 0)]
+        (recur (filter #(= (nth % bit) keep-value) remaining)
+               (inc bit))))))
+
 (defn part2 [input]
-  (let [oxygen-generator-rating (loop [remaining input
-                                       bit 0]
-                                  (if (= (count remaining) 1)
-                                    (bin-to-int (first remaining))
-                                    (let [number-count (count remaining)
-                                          half-number-count (int (Math/ceil (/ number-count 2)))
-                                          number-of-ones (apply map + remaining)
-                                          keep-value (if (>= (nth number-of-ones bit) half-number-count) 1 0)]
-                                      (recur (filter #(= (nth % bit) keep-value) remaining)
-                                             (inc bit)))))
-        co2-scrubber-rating (loop [remaining input
-                                   bit 0]
-                              (if (= (count remaining) 1)
-                                (bin-to-int (first remaining))
-                                (let [number-count (count remaining)
-                                      half-number-count (int (Math/ceil (/ number-count 2)))
-                                      number-of-ones (apply map + remaining)
-                                      keep-value (if (< (nth number-of-ones bit) half-number-count) 1 0)]
-                                  (recur (filter #(= (nth % bit) keep-value) remaining)
-                                         (inc bit)))))]
+  (let [oxygen-generator-rating (calculate-rating input >=)
+        co2-scrubber-rating (calculate-rating input <)]
     (* oxygen-generator-rating co2-scrubber-rating)))
 
 (def puzzle
