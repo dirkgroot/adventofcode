12d5edc6b6c8dd67b2edf575e6dc17d707ce2324
Dirk Groot <d.groot@avisi.nl>
2021-12-04 10:24:17 +0100

    Day 4 - first rough version which gets the job done

--- src/adventofcode/year2021/day04.clj
+++ src/adventofcode/year2021/day04.clj
@@ -1,14 +1,73 @@
 (ns adventofcode.year2021.day04
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse-int [str]
+  (Integer/parseInt str))
+
+(defn parse-random-numbers [input]
+  (map parse-int (str/split input #",")))
+
+(defn parse-boards [input]
+  (map #(hash-map :numbers (vec (map parse-int (str/split (str/trim %) #"[ \n]+")))
+                  :marked #{})
+       input))
 
 (defn parse-input [input]
-  :todo)
+  (let [chunks (str/split input #"\n\n")
+        random-numbers-string (first chunks)
+        board-strings (rest chunks)]
+    [(parse-random-numbers random-numbers-string) (parse-boards board-strings)]))
 
-(defn part1 [input]
-  :todo)
+(defn mark-board [board number]
+  (update-in board [:marked]
+             #(if (some (partial = number) (board :numbers))
+                (conj % number)
+                %)))
 
-(defn part2 [input]
-  :todo)
+(defn rows [numbers]
+  (partition 5 numbers))
+
+(defn cols [numbers]
+  (partition 5 (apply interleave (rows numbers))))
+
+(defn is-winner? [drawn board]
+  (let [any? (complement not-any?)
+        rows (map set (rows board))
+        cols (map set (cols board))]
+    (or (any? #(set/subset? % drawn) rows)
+        (any? #(set/subset? % drawn) cols))))
+
+(defn find-winners [boards]
+  (filter #(is-winner? (:marked %) (:numbers %)) boards))
+
+(defn find-winner [boards]
+  (first (find-winners boards)))
+
+(defn sum-of-unmarked [{numbers :numbers marked :marked}]
+  (reduce + (set/difference (set numbers) marked)))
+
+(defn part1 [[numbers boards]]
+  (loop [boards boards
+         [n & ns] numbers]
+    (let [updated-boards (map #(mark-board % n) boards)
+          winner (find-winner updated-boards)]
+      (if (nil? winner)
+        (recur updated-boards ns)
+        (* n (sum-of-unmarked winner))))))
+
+(defn part2 [[numbers boards]]
+  (loop [boards boards
+         [n & ns] numbers
+         last-winner nil
+         winning-move nil]
+    (if (nil? n)
+      (* winning-move (sum-of-unmarked last-winner))
+      (let [updated-boards (map #(mark-board % n) boards)
+            winners (find-winners updated-boards)]
+        (recur (remove #(some (partial = %) winners) updated-boards) ns
+               (if (not (empty? winners)) (first winners) last-winner)
+               (if (not (empty? winners)) n winning-move))))))
 
 (def puzzle
   {:year        2021
@@ -16,5 +75,5 @@
    :parse-input parse-input
    :part1       part1
    :part2       part2
-   :answer1     :todo
-   :answer2     :todo})
+   :answer1     12796
+   :answer2     18063})
