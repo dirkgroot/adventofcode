86fbebb2daa5ce8264cf5e45a9d0145d860c79e7
Dirk Groot <d.groot@avisi.nl>
2021-12-08 20:49:46 +0100

    Day 8 - simpler solution for day 2

--- src/adventofcode/year2021/day08.clj
+++ src/adventofcode/year2021/day08.clj
@@ -1,7 +1,6 @@
 (ns adventofcode.year2021.day08
   (:require [clojure.pprint :refer :all]
-            [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.string :as str]))
 
 (defn parse-input [input]
   (doall
@@ -18,30 +17,29 @@
 (defn part1 [input]
   (reduce + (map #(count-1478 %) input)))
 
-(defn deduce [patterns]
-  (loop [[test & rest] [1 7 4 8 9 6 2 3 5 0]
-         acc {}]
-    (if (nil? test)
-      acc
-      (recur rest
-             (assoc acc
-               test
-               (case test
-                 1 (first (filter #(= (count %) 2) patterns))
-                 7 (first (filter #(= (count %) 3) patterns))
-                 4 (first (filter #(= (count %) 4) patterns))
-                 8 (first (filter #(= (count %) 7) patterns))
-                 9 (first (filter #(and (= (count %) 6) (= 1 (count (set/difference % (set/union (acc 4) (acc 7)))))) patterns))
-                 6 (first (filter #(and (= (count %) 6) (not= % (acc 9)) (not (set/subset? (acc 7) %))) patterns))
-                 2 (first (filter #(and (= (count %) 5) (not (set/subset? % (acc 9)))) patterns))
-                 5 (first (filter #(and (= (count %) 5) (set/subset? % (acc 6))) patterns))
-                 3 (first (filter #(and (= (count %) 5) (not= % (acc 5)) (set/subset? % (acc 9)) (not (set/subset? % (acc 6)))) patterns))
-                 0 (first (filter #(not (some (fn [[_ v]] (= % v)) acc)) patterns))))))))
+(def translation-table {"4677889" 8
+                        "467789"  6
+                        "467889"  0
+                        "47788"   2
+                        "677889"  9
+                        "67789"   5
+                        "6789"    4
+                        "77889"   3
+                        "889"     7
+                        "89"      1})
+
+(defn translate-output [{output :output translation-map :map}]
+  (->> (map translation-map output)
+       (map translation-table)
+       (reduce (fn [acc n] (+ (* acc 10) n)))))
+
+(defn map-to-frequencies [patterns]
+  (let [freqs (frequencies (reduce str patterns))]
+    (reduce #(assoc %1 %2 (reduce str (sort (map freqs %2)))) {} patterns)))
 
 (defn part2 [input]
-  (->> (map #(assoc % :deduction (set/map-invert (deduce (% :patterns)))) input)
-       (map #(map (% :deduction) (% :output)))
-       (map #(reduce (fn [acc n] (+ (* acc 10) n)) %))
+  (->> (map #(assoc % :map (map-to-frequencies (% :patterns))) input)
+       (map translate-output)
        (reduce +)))
 
 (def puzzle
--- test/adventofcode/year2021/day08_test.clj
+++ test/adventofcode/year2021/day08_test.clj
@@ -17,19 +17,6 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 "))
 
-(deftest deduce-test
-  (is (= (day/deduce (map set ["acedgfb" "cdfbe" "gcdfa" "fbcad" "dab" "cefabd" "cdfgeb" "eafb" "cagedb" "ab"]))
-         {0 #{\c \a \g \e \d \b}
-          1 #{\a \b}
-          2 #{\g \c \d \f \a}
-          3 #{\f \b \c \a \d}
-          4 #{\e \a \f \b}
-          5 #{\c \d \f \b \e}
-          6 #{\c \d \f \g \e \b}
-          7 #{\d \a \b}
-          8 #{\a \c \e \d \g \f \b}
-          9 #{\c \e \f \a \b \d}})))
-
 (deftest part1-example
   (is (= (day/part1 example-input) 26)))
 
