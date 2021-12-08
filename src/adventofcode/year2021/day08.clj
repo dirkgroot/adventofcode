c7570eadb89a9d526278454a03537508519753e7
Dirk Groot <d.groot@avisi.nl>
2021-12-08 16:33:28 +0100

    Day 8

--- src/adventofcode/year2021/day08.clj
+++ src/adventofcode/year2021/day08.clj
@@ -1,14 +1,49 @@
 (ns adventofcode.year2021.day08
-  (:require [clojure.string :as str]))
+  (:require [clojure.pprint :refer :all]
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn parse-input [input]
-  :todo)
+  (doall
+    (->> (str/split-lines input)
+         (map #(str/split % #"\s+\|\s+"))
+         (map (fn [[patterns-str output-str]] {:patterns-str (str/replace patterns-str #"\s+" "")
+                                               :patterns     (map set (str/split patterns-str #" "))
+                                               :output       (map set (str/split output-str #" "))})))))
+
+(defn count-1478 [input]
+  (->> (input :output)
+       (filter #(contains? #{2 4 3 7} (count %)))
+       (count)))
 
 (defn part1 [input]
-  :todo)
+  (reduce + (map #(count-1478 %) input)))
+
+(defn deduce [patterns]
+  (loop [[test & rest] [1 7 4 8 9 6 2 3 5 0]
+         acc {}]
+    (if (nil? test)
+      acc
+      (recur rest
+             (assoc acc
+               test
+               (case test
+                 1 (first (filter #(= (count %) 2) patterns))
+                 7 (first (filter #(= (count %) 3) patterns))
+                 4 (first (filter #(= (count %) 4) patterns))
+                 8 (first (filter #(= (count %) 7) patterns))
+                 9 (first (filter #(and (= (count %) 6) (= 1 (count (set/difference % (set/union (acc 4) (acc 7)))))) patterns))
+                 6 (first (filter #(and (= (count %) 6) (not= % (acc 9)) (not (set/subset? (acc 7) %))) patterns))
+                 2 (first (filter #(and (= (count %) 5) (not (set/subset? % (acc 9)))) patterns))
+                 5 (first (filter #(and (= (count %) 5) (set/subset? % (acc 6))) patterns))
+                 3 (first (filter #(and (= (count %) 5) (not= % (acc 5)) (set/subset? % (acc 9)) (not (set/subset? % (acc 6)))) patterns))
+                 0 (first (filter #(not (some (fn [[_ v]] (= % v)) acc)) patterns))))))))
 
 (defn part2 [input]
-  :todo)
+  (->> (map #(assoc % :deduction (set/map-invert (deduce (% :patterns)))) input)
+       (map #(map (% :deduction) (% :output)))
+       (map #(reduce (fn [acc n] (+ (* acc 10) n)) %))
+       (reduce +)))
 
 (def puzzle
   {:year        2021
@@ -16,5 +51,5 @@
    :parse-input parse-input
    :part1       part1
    :part2       part2
-   :answer1     :todo
-   :answer2     :todo})
+   :answer1     352
+   :answer2     936117})
--- test/adventofcode/year2021/day08_test.clj
+++ test/adventofcode/year2021/day08_test.clj
@@ -1,12 +1,37 @@
 (ns adventofcode.year2021.day08-test
   (:require [clojure.test :refer :all]
+            [clojure.pprint :refer :all]
             [adventofcode.year2021.day08 :as day]))
 
 (def example-input
-  :todo)
+  (day/parse-input
+    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+"))
+
+(deftest deduce-test
+  (is (= (day/deduce (map set ["acedgfb" "cdfbe" "gcdfa" "fbcad" "dab" "cefabd" "cdfgeb" "eafb" "cagedb" "ab"]))
+         {0 #{\c \a \g \e \d \b}
+          1 #{\a \b}
+          2 #{\g \c \d \f \a}
+          3 #{\f \b \c \a \d}
+          4 #{\e \a \f \b}
+          5 #{\c \d \f \b \e}
+          6 #{\c \d \f \g \e \b}
+          7 #{\d \a \b}
+          8 #{\a \c \e \d \g \f \b}
+          9 #{\c \e \f \a \b \d}})))
 
 (deftest part1-example
-  (is (= (day/part1 example-input) :todo)))
+  (is (= (day/part1 example-input) 26)))
 
 (deftest part2-example
-  (is (= (day/part2 example-input) :todo)))
+  (is (= (day/part2 example-input) 61229)))
