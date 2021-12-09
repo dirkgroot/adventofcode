f8a260c3c29da61a87c7af693dd92ff02e16ccac
Dirk Groot <d.groot@avisi.nl>
2021-12-09 08:27:50 +0100

    Day 9 - will cleanup and refactor later

--- src/adventofcode/year2021/day09.clj
+++ src/adventofcode/year2021/day09.clj
@@ -1,14 +1,71 @@
 (ns adventofcode.year2021.day09
-  (:require [clojure.string :as str]))
+  (:require [clojure.pprint :refer :all]
+            [clojure.string :as str]))
+
+(defn parse-map [input]
+  (->> (str/split-lines input)
+       (map (fn [line] (map #(Integer/parseInt (str %)) line)))))
 
 (defn parse-input [input]
-  :todo)
+  (let [height-map (parse-map input)
+        nx (count (nth height-map 0))
+        ny (count height-map)
+        dummy-row (repeat (+ nx 2) 9)]
+    {:heights (->> height-map
+                   (map #(cons 9 (concat % '(9))))
+                   (#(cons dummy-row (concat % [dummy-row]))))
+     :nx      nx
+     :ny      ny}))
+
+(defn neighbors [x y]
+  [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]])
+
+(defn neighbor-heights [heights x y]
+  [(nth (nth heights y) (dec x))
+   (nth (nth heights y) (inc x))
+   (nth (nth heights (dec y)) x)
+   (nth (nth heights (inc y)) x)])
+
+(defn is-low-point [{heights :heights} [x y]]
+  (let [height (nth (nth heights y) x)
+        neighbors (neighbor-heights heights x y)]
+    (every? #(< height %) neighbors)))
+
+(defn all-coords [nx ny]
+  (apply concat (map (fn [y] (map (fn [x] [x y]) (range 1 (inc nx)))) (range 1 (inc ny)))))
+
+(defn get-height [heights x y]
+  (nth (nth heights y) x))
 
 (defn part1 [input]
-  :todo)
+  (let [heights (input :heights)
+        nx (input :nx)
+        ny (input :ny)]
+    (->> (all-coords nx ny)
+         (filter #(is-low-point input %))
+         (map (fn [[x y]] (get-height heights x y)))
+         (map inc)
+         (reduce +))))
+
+(defn get-basin [heights [x y]]
+  (lazy-seq
+    (distinct
+      (let [height (get-height heights x y)
+            neighbors (neighbors x y)
+            basin-neighbors (filter (fn [[nx ny]] (> 9 (get-height heights nx ny) height)) neighbors)]
+        (cons [x y] (apply concat (map #(get-basin heights %) basin-neighbors)))))))
 
 (defn part2 [input]
-  :todo)
+  (let [heights (input :heights)
+        nx (input :nx)
+        ny (input :ny)]
+    (->> (all-coords nx ny)
+         (filter #(is-low-point input %))
+         (map #(get-basin heights %))
+         (sort-by #(- (count %)))
+         (take 3)
+         (map #(count %))
+         (reduce *))))
 
 (def puzzle
   {:year        2021
@@ -16,5 +73,5 @@
    :parse-input parse-input
    :part1       part1
    :part2       part2
-   :answer1     :todo
-   :answer2     :todo})
+   :answer1     530
+   :answer2     1019494})
--- test/adventofcode/year2021/day09_test.clj
+++ test/adventofcode/year2021/day09_test.clj
@@ -3,10 +3,14 @@
             [adventofcode.year2021.day09 :as day]))
 
 (def example-input
-  :todo)
+  (day/parse-input "2199943210
+3987894921
+9856789892
+8767896789
+9899965678"))
 
 (deftest part1-example
-  (is (= (day/part1 example-input) :todo)))
+  (is (= (day/part1 example-input) 15)))
 
 (deftest part2-example
-  (is (= (day/part2 example-input) :todo)))
+  (is (= (day/part2 example-input) 1134)))
