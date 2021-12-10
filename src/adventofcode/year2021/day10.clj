feb555770e57b84a9a3f21110d1cca9e78621cfa
Dirk Groot <d.groot@avisi.nl>
2021-12-10 18:16:58 +0100

    Day 10 - refactoring

--- src/adventofcode/year2021/day10.clj
+++ src/adventofcode/year2021/day10.clj
@@ -6,33 +6,17 @@
 (defn parse-input [input]
   (str/split-lines input))
 
-(def matching-brace
-  {\( \)
-   \[ \]
-   \{ \}
-   \< \>})
-
 (defn open-close-pairs
   ([line] (open-close-pairs line nil))
   ([[c & cs] stack]
    (lazy-seq (cond (and (nil? c) (empty? stack)) nil
                    (contains? #{\( \[ \{ \<} c) (open-close-pairs cs (cons c stack))
-                   (contains? #{\) \] \} \>} c) (cons {:first (first stack) :second c}
-                                                      (open-close-pairs cs (rest stack)))
-                   :else (cons {:first (first stack) :second (matching-brace (first stack)) :added true}
-                               (open-close-pairs nil (rest stack)))))))
+                   (contains? #{\) \] \} \>} c) (cons {:first (first stack) :second c} (open-close-pairs cs (rest stack)))
+                   :else (cons {:first (first stack) :open true} (open-close-pairs nil (rest stack)))))))
 
-(defn syntax-error-score [c]
-  (case c
-    \) 3
-    \] 57
-    \} 1197
-    \> 25137))
-
-(def valid-pairs #{[\( \)] [\[ \]] [\{ \}] [\< \>]})
-
-(defn is-valid? [{first :first second :second}]
-  (contains? valid-pairs [first second]))
+(defn is-valid? [{first :first second :second open :open}]
+  (or open (contains? #{[\( \)] [\[ \]] [\{ \}] [\< \>]}
+                      [first second])))
 
 (defn first-invalid-pair [pairs]
   (first (filter #(not (is-valid? %)) pairs)))
@@ -42,28 +26,18 @@
        (map first-invalid-pair)
        (filter (complement nil?))
        (map :second)
-       (map syntax-error-score)
+       (map {\) 3 \] 57 \} 1197 \> 25137})
        (reduce +)))
 
-(defn autocomplete-score [c]
-  (case c
-    \) 1
-    \] 2
-    \} 3
-    \> 4))
-
 (defn calculate-autocomplete-score [pairs]
-  (->> (filter #(contains? % :added) pairs)
-       (map :second)
-       (map autocomplete-score)
+  (->> (filter #(contains? % :open) pairs)
+       (map :first)
+       (map {\( 1 \[ 2 \{ 3 \< 4})
        (reduce #(+ (* %1 5) %2))))
-
-(defn only-valid-lines [pairs]
-  (every? is-valid? pairs))
 
 (defn part2 [input]
   (->> (map open-close-pairs input)
-       (filter only-valid-lines)
+       (filter #(every? is-valid? %))
        (map calculate-autocomplete-score)
        (median)))
 
