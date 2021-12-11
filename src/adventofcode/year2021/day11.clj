8b0a7b396aeb58f4860ee91e2a8e3ae193a5c19d
Dirk Groot <d.groot@avisi.nl>
2021-12-11 09:55:28 +0100

    Day 11

--- src/adventofcode/year2021/day11.clj
+++ src/adventofcode/year2021/day11.clj
@@ -1,14 +1,55 @@
 (ns adventofcode.year2021.day11
-  (:require [clojure.string :as str]))
+  (:require [clojure.pprint :refer :all]
+            [clojure.string :as str]))
 
 (defn parse-input [input]
-  :todo)
+  (->> (str/split-lines input)
+       (map #(str/split % #""))
+       (map #(map (fn [n] (Integer/parseInt n)) %))
+       (flatten)
+       (vec)))
+
+(def neighbors [-11 -10 -9 -1 1 9 10 11])
+(def neighbors-left-edge [-10 -9 1 10 11])
+(def neighbors-right-edge [-11 -10 -1 9 10])
+
+(defn get-neighbors [index]
+  (->> (cond (= (mod index 10) 0) neighbors-left-edge
+             (= (mod index 10) 9) neighbors-right-edge
+             :else neighbors)
+       (map #(+ index %))
+       (filter #(<= 0 % 99))))
+
+(defn simulate-flashes [grid]
+  (let [flashed          (filter #(> (get grid %) 9) (range 0 100))
+        neighbors-counts (frequencies (flatten (map get-neighbors flashed)))]
+    (if (empty? flashed)
+      grid
+      (simulate-flashes (vec (map-indexed (fn [idx energy] (cond (> energy 9) 0
+                                                                 (zero? energy) 0
+                                                                 :else (+ energy (get neighbors-counts idx 0))))
+                                          grid))))))
+
+(defn step [grid]
+  (->> (vec (map inc grid))
+       (simulate-flashes)))
+
+(defn simulation [grid]
+  (lazy-seq
+    (let [after-step (step grid)]
+      (cons after-step (simulation after-step)))))
 
 (defn part1 [input]
-  :todo)
+  (->> (simulation input)
+       (take 100)
+       (map #(count (filter zero? %)))
+       (reduce +)))
 
 (defn part2 [input]
-  :todo)
+  (->> (simulation input)
+       (take-while #(not-every? zero? %))
+       (count)
+       (inc)))
 
 (def puzzle
   {:year        2021
@@ -16,5 +57,5 @@
    :parse-input parse-input
    :part1       part1
    :part2       part2
-   :answer1     :todo
-   :answer2     :todo})
+   :answer1     1749
+   :answer2     285})
--- test/adventofcode/year2021/day11_test.clj
+++ test/adventofcode/year2021/day11_test.clj
@@ -3,10 +3,44 @@
             [adventofcode.year2021.day11 :as day]))
 
 (def example-input
-  :todo)
+  (day/parse-input "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"))
+
+(deftest parse-input
+  (is (= example-input [5 4 8 3 1 4 3 2 2 3
+                        2 7 4 5 8 5 4 7 1 1
+                        5 2 6 4 5 5 6 1 7 3
+                        6 1 4 1 3 3 6 1 4 6
+                        6 3 5 7 3 8 5 4 7 8
+                        4 1 6 7 5 2 4 6 4 5
+                        2 1 7 6 8 4 1 7 2 1
+                        6 8 8 2 8 8 1 1 3 4
+                        4 8 4 6 8 4 8 5 5 4
+                        5 2 8 3 7 5 1 5 2 6])))
+
+(deftest blaat
+  (day/part1 (day/parse-input "0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000900000
+0000000000
+0000000000
+0000000000
+0000000000
+")))
 
 (deftest part1-example
-  (is (= (day/part1 example-input) :todo)))
+  (is (= (day/part1 example-input) 1656)))
 
 (deftest part2-example
-  (is (= (day/part2 example-input) :todo)))
+  (is (= (day/part2 example-input) 195)))
