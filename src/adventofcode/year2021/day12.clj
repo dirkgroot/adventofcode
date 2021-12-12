e3ef1f0020d6c53c3366051af3b0c98a40caa8b6
Dirk Groot <d.groot@avisi.nl>
2021-12-12 10:43:27 +0100

    Day 12 - fix spelling

--- src/adventofcode/year2021/day12.clj
+++ src/adventofcode/year2021/day12.clj
@@ -13,8 +13,8 @@
                                        :small?       (nil? (re-matches #"[A-Z]+" start))})))
                {})))
 
-(defn count-foutes
-  ([caves small-visits-limit] (count-foutes caves small-visits-limit 0 (caves "start")))
+(defn count-routes
+  ([caves small-visits-limit] (count-routes caves small-visits-limit 0 (caves "start")))
   ([caves small-visits-limit max-small-visits {name :name connected-to :connected-to visited :visited small? :small?}]
    (cond (= name "end") 1
          (and (= name "start") (= visited 1)) 0
@@ -23,14 +23,14 @@
          :else (let [updated-visited               (inc visited)
                      updated-max-small-cave-visits (if small? (max max-small-visits updated-visited) max-small-visits)
                      updated-caves                 (update caves name #(assoc % :visited updated-visited))]
-                 (reduce + (map #(count-foutes updated-caves small-visits-limit updated-max-small-cave-visits (caves %))
+                 (reduce + (map #(count-routes updated-caves small-visits-limit updated-max-small-cave-visits (caves %))
                                 connected-to))))))
 
 (defn part1 [input]
-  (count-foutes input 1))
+  (count-routes input 1))
 
 (defn part2 [input]
-  (count-foutes input 2))
+  (count-routes input 2))
 
 (def puzzle
   {:year        2021
