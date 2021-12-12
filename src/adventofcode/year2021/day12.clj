6c511f7f1758dda291daed09707d9c6e66c31327
Dirk Groot <d.groot@avisi.nl>
2021-12-12 09:47:47 +0100

    Day 12

--- src/adventofcode/year2021/day12.clj
+++ src/adventofcode/year2021/day12.clj
@@ -1,14 +1,37 @@
 (ns adventofcode.year2021.day12
-  (:require [clojure.string :as str]))
+  (:require [clojure.pprint :refer :all]))
 
 (defn parse-input [input]
-  :todo)
+  (->> (re-seq #"(.*)-(.*)" input)
+       (map (fn [[_ start end]] [start end end start]))
+       (flatten)
+       (partition 2)
+       (reduce (fn [caves [start end]]
+                 (if (contains? caves start)
+                   (update caves start (fn [cave] (update cave :connected-to #(conj % end))))
+                   (assoc caves start {:name         start
+                                       :connected-to #{end}
+                                       :visited      0
+                                       :small?       (nil? (re-matches #"[A-Z]+" start))})))
+               {})))
+
+(defn find-foutes
+  ([caves max-visits-small] (find-foutes caves max-visits-small [] (caves "start")))
+  ([caves max-visits-small route {name :name connected-to :connected-to visited :visited small? :small? :as cave}]
+   (cond (= name "end") [(conj route cave)]
+         (and (= visited 1) (= name "start")) nil
+         (and (= visited 1) small? (some #(and (% :small?) (= max-visits-small (% :visited))) route)) nil
+         (and (= visited max-visits-small) small?) nil
+         :else (let [updated-cave  (update cave :visited inc)
+                     updated-caves (assoc caves name updated-cave)]
+                 (apply concat (map #(find-foutes updated-caves max-visits-small (conj route updated-cave) (caves %))
+                                    connected-to))))))
 
 (defn part1 [input]
-  :todo)
+  (count (find-foutes input 1)))
 
 (defn part2 [input]
-  :todo)
+  (count (find-foutes input 2)))
 
 (def puzzle
   {:year        2021
@@ -16,5 +39,5 @@
    :parse-input parse-input
    :part1       part1
    :part2       part2
-   :answer1     :todo
-   :answer2     :todo})
+   :answer1     5576
+   :answer2     152837})
--- test/adventofcode/year2021/day12_test.clj
+++ test/adventofcode/year2021/day12_test.clj
@@ -3,10 +3,24 @@
             [adventofcode.year2021.day12 :as day]))
 
 (def example-input
-  :todo)
+  (day/parse-input "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"))
+
+(deftest parse-input
+  (is (= example-input {"start" {:name "start" :visited 0 :connected-to #{"A" "b"} :small? true}
+                        "c"     {:name "c" :visited 0 :connected-to #{"A"} :small? true}
+                        "A"     {:name "A" :visited 0 :connected-to #{"c" "start" "b" "end"} :small? false}
+                        "b"     {:name "b" :visited 0 :connected-to #{"start" "A" "d" "end"} :small? true}
+                        "d"     {:name "d" :visited 0 :connected-to #{"b"} :small? true}
+                        "end"   {:name "end" :visited 0 :connected-to #{"A" "b"} :small? true}})))
 
 (deftest part1-example
-  (is (= (day/part1 example-input) :todo)))
+  (is (= (day/part1 example-input) 10)))
 
 (deftest part2-example
-  (is (= (day/part2 example-input) :todo)))
+  (is (= (day/part2 example-input) 36)))
