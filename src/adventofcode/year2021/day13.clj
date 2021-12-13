601e36cae8039577fb1d3eb831ebcf58b5ee792e
Dirk Groot <d.groot@avisi.nl>
2021-12-13 07:54:52 +0100

    Day 13

--- src/adventofcode/year2021/day13.clj
+++ src/adventofcode/year2021/day13.clj
@@ -1,11 +1,38 @@
 (ns adventofcode.year2021.day13
-  (:require [clojure.string :as str]))
+  (:require [clojure.pprint :refer :all]
+            [clojure.string :as str]))
+
+(defn parse-dots [dots]
+  (doall (->> (re-seq #"(\d+),(\d+)" dots)
+              (map (fn [[_ x y]] [(Integer/parseInt x) (Integer/parseInt y)])))))
+
+(defn parse-folds [folds]
+  (doall (->> (re-seq #"fold along (x|y)=(\d+)" folds)
+              (map (fn [[_ xy pos]] [(if (= xy "x") :horizontal :vertical) (Integer/parseInt pos)])))))
 
 (defn parse-input [input]
-  :todo)
+  (let [[dots folds] (str/split input #"\n\n")]
+    {:dots (set (parse-dots dots)) :folds (parse-folds folds)}))
 
-(defn part1 [input]
-  :todo)
+(defn fold [n pos]
+  (if (> n pos) (- n (* (- n pos) 2)) n))
 
-(defn part2 [input]
-  :todo)
+(defn create-folds [dots [[direction pos] & fs]]
+  (when direction
+    (let [folded (set (case direction
+                        :horizontal (map (fn [[x y]] [(fold x pos) y]) dots)
+                        :vertical (map (fn [[x y]] [x (fold y pos)]) dots)))]
+      (cons folded (create-folds folded fs)))))
+
+(defn part1 [{dots :dots folds :folds}]
+  (count (first (create-folds dots folds))))
+
+(defn visualize [dots]
+  (let [max-x (apply max (map first dots)) max-y (apply max (map second dots))]
+    (for [y (range 0 (inc max-y))]
+      (for [x (range 0 (inc max-x))]
+        (if (contains? dots [x y]) "#" ".")))))
+
+(defn part2 [{dots :dots folds :folds}]
+  (let [final-dots (last (create-folds dots folds))]
+    (str/join "\n" (map #(apply str %) (visualize final-dots)))))
--- test/adventofcode/year2021/day13_test.clj
+++ test/adventofcode/year2021/day13_test.clj
@@ -1,16 +1,50 @@
 (ns adventofcode.year2021.day13-test
   (:require [clojure.test :refer :all]
             [adventofcode.year2021.day13 :as day]
-            [adventofcode.support.test :as test]))
+            [adventofcode.support.test :as test]
+            [clojure.string :as str]))
 
 (def example-input
-  :todo)
+  (day/parse-input "6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"))
 
 (deftest part1-example
-  (is (= (day/part1 example-input) :todo)))
+  (is (= (day/part1 example-input) 17)))
 
 (deftest part2-example
-  (is (= (day/part2 example-input) :todo)))
+  (is (= (day/part2 example-input)
+         (str/join "\n" ["#####"
+                         "#...#"
+                         "#...#"
+                         "#...#"
+                         "#####"]))))
+
+(def part2-solution
+  (str/join "\n" ["..##.###..####..##..#..#..##..#..#.###."
+                  "...#.#..#....#.#..#.#..#.#..#.#..#.#..#"
+                  "...#.#..#...#..#....#..#.#..#.#..#.#..#"
+                  "...#.###...#...#....#..#.####.#..#.###."
+                  "#..#.#....#....#..#.#..#.#..#.#..#.#.#."
+                  ".##..#....####..##...##..#..#..##..#..#"]))
 
 (deftest solution
-  (test/test-puzzle 2021 13))
+  (test/test-puzzle 2021 13 655 part2-solution))
