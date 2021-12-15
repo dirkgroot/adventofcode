93a0de1965f3b95d8151a489da39fd51b1a9b6a6
Dirk Groot <d.groot@avisi.nl>
2021-12-15 19:11:44 +0100

    Day 15 - refactoring

--- src/adventofcode/year2021/day15.clj
+++ src/adventofcode/year2021/day15.clj
@@ -27,27 +27,28 @@
                (cons current (reconstruct-path came-from current-came-from (came-from current-came-from)))
                nil))))
 
+(defn evaluate-neighbors [grid [u dist-u] dist prev queue]
+  (loop [[v & rest-v] (neighbors grid u)
+         dist  dist
+         prev  prev
+         queue queue]
+    (if (nil? v)
+      [dist prev queue]
+      (let [alt (+ dist-u (risk grid v))]
+        (if (< alt (get dist v infinity))
+          (recur rest-v (assoc dist v alt) (assoc prev v u) (assoc queue v alt))
+          (recur rest-v dist prev queue))))))
+
 (defn dijkstra [grid start goal]
   (loop [dist  {start 0}
          prev  {}
          queue (priority-map start 0)]
-    (let [[u dist-u] (peek queue)
+    (let [u     (peek queue)
           queue (pop queue)]
-      (if (= u goal)
+      (if (= (first u) goal)
         (reconstruct-path prev goal)
-        (letfn [(evaluate-neighbors []
-                  (loop [[v & rest-v] (neighbors grid u)
-                         dist  dist
-                         prev  prev
-                         queue queue]
-                    (if (nil? v)
-                      [dist prev queue]
-                      (let [alt (+ dist-u (risk grid v))]
-                        (if (< alt (get dist v infinity))
-                          (recur rest-v (assoc dist v alt) (assoc prev v u) (assoc queue v alt))
-                          (recur rest-v dist prev queue))))))]
-          (let [[dist prev queue] (evaluate-neighbors)]
-            (recur dist prev queue)))))))
+        (let [[dist prev queue] (time (evaluate-neighbors grid u dist prev queue))]
+          (recur dist prev queue))))))
 
 (defn lowest-total-risk [grid]
   (let [start [0 0]
