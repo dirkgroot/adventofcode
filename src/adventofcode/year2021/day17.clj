49051a529527a1cbcbdfaed3d62690ceea7cd893
Dirk Groot <d.groot@avisi.nl>
2021-12-17 17:24:45 +0100

    Day 17

--- src/adventofcode/year2021/day17.clj
+++ src/adventofcode/year2021/day17.clj
@@ -1,11 +1,19 @@
-(ns adventofcode.year2021.day17
-  (:require [clojure.string :as str]))
+(ns adventofcode.year2021.day17)
 
 (defn parse-input [input]
-  :todo)
+  (let [[_ min-x max-x min-y max-y] (re-matches #"target area: x=(\d+)..(\d+), y=(-?\d+)..(-?\d+)\n?" input)]
+    [(Integer/parseInt min-x) (Integer/parseInt max-x) (Integer/parseInt min-y) (Integer/parseInt max-y)]))
 
-(defn part1 [input]
-  :todo)
+(defn xy-velocities [min-x max-x min-y max-y]
+  (for [vx (range 1 (inc max-x)) vy (range min-y (- min-y))]
+    (loop [x 0 y 0 highest-y 0 vx vx vy vy]
+      (cond (and (<= min-x x max-x) (<= min-y y max-y)) {:hit true :highest-y highest-y}
+            (or (> x max-x) (< y min-y)) {:hit false}
+            :else (recur (+ x vx) (+ y vy) (max y highest-y) (max (dec vx) 0) (dec vy))))))
 
-(defn part2 [input]
-  :todo)
+(defn part1 [[min-x max-x min-y max-y]]
+  (let [hits (filter :hit (xy-velocities min-x max-x min-y max-y))]
+    (:highest-y (apply max-key :highest-y hits))))
+
+(defn part2 [[min-x max-x min-y max-y]]
+  (count (filter :hit (xy-velocities min-x max-x min-y max-y))))
--- test/adventofcode/year2021/day17_test.clj
+++ test/adventofcode/year2021/day17_test.clj
@@ -4,13 +4,13 @@
             [adventofcode.support.test :as test]))
 
 (def example-input
-  :todo)
+  (day/parse-input "target area: x=20..30, y=-10..-5"))
 
 (deftest part1-example
-  (is (= (day/part1 example-input) :todo)))
+  (is (= (day/part1 example-input) 45)))
 
 (deftest part2-example
-  (is (= (day/part2 example-input) :todo)))
+  (is (= (day/part2 example-input) 112)))
 
 (deftest solution
-  (test/test-puzzle 2021 17))
+  (test/test-puzzle 2021 17 3570 1919))
