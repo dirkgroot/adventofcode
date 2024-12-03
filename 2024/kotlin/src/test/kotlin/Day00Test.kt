e73761b5817c5362c022362463898f49a0161f8e
Dirk Groot <dirkgroot77@gmail.com>
2024-12-03 16:31:38 +0100

    kt_2024: Adjust solution template

--- 2024/kotlin/src/test/kotlin/Day00Test.kt
+++ 2024/kotlin/src/test/kotlin/Day00Test.kt
@@ -4,11 +4,14 @@ import util.input
 import util.invokedWith
 import org.junit.jupiter.api.Test
 
+private fun part1(input: String) = input.length
+
+private fun part2(input: String) = input.length
+
 class Day00Test {
     @Test
-    fun `part1 solution`() {
-        assertThat(::part1 invokedWith input(0)).isEqualTo(21)
-    }
+    fun `part1 solution`() = assertThat(::part1 invokedWith input(0)).isEqualTo(21)
 
-    fun part1(input: String) = input.length
+    @Test
+    fun `part2 solution`() = assertThat(::part2 invokedWith input(0)).isEqualTo(21)
 }
