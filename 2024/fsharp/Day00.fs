357b214a0e0364b086f7f6f8e2d97f0557a3662e
Dirk Groot <dirkgroot77@gmail.com>
2024-12-04 18:32:59 +0100

    fs_2024: Update template

--- 2024/fsharp/Day00.fs
+++ 2024/fsharp/Day00.fs
@@ -12,18 +12,17 @@ let part2 (input: string) = input |> int |> (fun i -> i * 2)
 [<Literal>]
 let DAY = 0
 
-[<Fact>]
-let ``part 1 - example`` () =
-    Day DAY |> withInput (Value "1") |> solvedWith part1 |> shouldEqual 1
+[<Literal>]
+let EXAMPLE = "1"
 
 [<Fact>]
-let ``part 1 - solution`` () =
-    Day DAY |> withInput FromFile |> solvedWith part1 |> shouldEqual 42
+let ``part 1 - example`` () = Day DAY |> withInput (Value EXAMPLE) |> solvedWith part1 |> shouldEqual 1
 
 [<Fact>]
-let ``part 2 - example`` () =
-    Day DAY |> withInput (Value "1") |> solvedWith part2 |> shouldEqual 2
+let ``part 1 - solution`` () = Day DAY |> withInput FromFile |> solvedWith part1 |> shouldEqual 42
 
 [<Fact>]
-let ``part 2 - solution`` () =
-    Day DAY |> withInput FromFile |> solvedWith part2 |> shouldEqual 84
+let ``part 2 - example`` () = Day DAY |> withInput (Value EXAMPLE) |> solvedWith part2 |> shouldEqual 2
+
+[<Fact>]
+let ``part 2 - solution`` () = Day DAY |> withInput FromFile |> solvedWith part2 |> shouldEqual 84
