31ac31d2ccad9006acfb1662b9c2c8b9b86100fb
Dirk Groot <dirkgroot77@gmail.com>
2024-12-08 10:00:12 +0100

    fs_2024_8: Refactoring

--- 2024/fsharp/Day08.fs
+++ 2024/fsharp/Day08.fs
@@ -5,19 +5,16 @@ open AoC.Puzzle
 open FsUnitTyped
 open Xunit
 
-type Coord = (struct (int * int))
-
 let parse (input: string) = input.Split('\n') |> Seq.toArray
 
 let antennas (grid: string array) =
     let indices = seq { 0 .. (grid.Length - 1) }
 
     indices
-    |> Seq.collect (fun y -> indices |> Seq.map (fun x -> grid[y][x], Coord(y, x)))
+    |> Seq.collect (fun y -> indices |> Seq.map (fun x -> grid[y][x], (y, x)))
     |> Seq.filter (fun (c, _) -> c <> '.')
     |> Seq.groupBy fst
-    |> Seq.map (fun (freq, b) -> freq, (Seq.map snd b |> Seq.toArray))
-    |> Map.ofSeq
+    |> Seq.map (fun (_, b) -> (Seq.map snd b))
 
 [<TailCall>]
 let rec antinodes acc finder antennas =
@@ -25,40 +22,33 @@ let rec antinodes acc finder antennas =
     | n :: rest -> antinodes (rest |> List.fold (fun ans c -> (finder n c) @ ans) acc) finder rest
     | _ -> acc
 
-let countAntinodes finder (grid: string array) =
-    let maxXY = grid.Length - 1
-    let findAll antennas = antennas |> Array.toList |> antinodes [] finder
-    let all = grid |> antennas |> Map.values |> Seq.map findAll |> Seq.collect id
-    let distinct = all |> Seq.distinct
+let isInGrid (grid: string array) (y, x) = let maxXY = grid.Length - 1 in y >= 0 && y <= maxXY && x >= 0 && x <= maxXY
 
-    distinct
-    |> Seq.filter (fun struct (y, x) -> y >= 0 && y <= maxXY && x >= 0 && x <= maxXY)
-    |> Seq.length
+let countAntinodes finder grid =
+    let findAll antennas = antennas |> Seq.toList |> antinodes [] finder
+    let allAntinodes = grid |> antennas |> Seq.map findAll |> Seq.collect id
+
+    allAntinodes |> Seq.filter (isInGrid grid) |> Seq.distinct |> Seq.length
 
 let part1 (input: string) =
-    let antinodesOfPair struct (y1, x1) struct (y2, x2) =
+    let antinodesOfPair (y1, x1) (y2, x2) =
         let diffX, diffY = x2 - x1, y2 - y1
-        [ Coord(y1 - diffY, x1 - diffX); Coord(y2 + diffY, x2 + diffX) ]
+        [ (y1 - diffY, x1 - diffX); (y2 + diffY, x2 + diffX) ]
 
     input |> parse |> countAntinodes antinodesOfPair
 
 let part2 (input: string) =
     let grid = input |> parse
-    let maxXY = grid.Length - 1
 
-    let antinodesOfPair (a as struct (y1, x1)) (b as struct (y2, x2)) =
+    let antinodesOfPair (a as (y1, x1)) (b as (y2, x2)) =
         let diffX, diffY = x2 - x1, y2 - y1
 
-        let nextAntinode fn struct (y, x) =
-            let yy, xx = fn y diffY, fn x diffX
+        let nextAntinode fn (y, x) =
+            let coord = (fn y diffY, fn x diffX)
+            if isInGrid grid coord then Some(coord, coord) else None
 
-            if (yy >= 0 && yy <= maxXY && xx >= 0 && xx <= maxXY) then
-                Some(Coord(yy, xx), Coord(yy, xx))
-            else
-                None
-
-        let antinodes1 = Seq.unfold (nextAntinode (-)) (Coord(y1, x1))
-        let antinodes2 = Seq.unfold (nextAntinode (+)) (Coord(y1, x1))
+        let antinodes1 = Seq.unfold (nextAntinode (-)) (y1, x1)
+        let antinodes2 = Seq.unfold (nextAntinode (+)) (y1, x1)
 
         a :: b :: ([ antinodes1; antinodes2 ] |> Seq.collect id |> Seq.toList)
 
@@ -83,7 +73,7 @@ let EXAMPLE =
 ............"
 
 [<Fact>]
-let ``example antennas`` () = parse EXAMPLE |> antennas |> Map.keys |> Seq.length |> shouldEqual 2
+let ``example antennas`` () = parse EXAMPLE |> antennas |> Seq.length |> shouldEqual 2
 
 [<Fact>]
 let ``part 1 - example`` () = Day DAY |> withInput (Value EXAMPLE) |> solvedWith part1 |> shouldEqual 14
