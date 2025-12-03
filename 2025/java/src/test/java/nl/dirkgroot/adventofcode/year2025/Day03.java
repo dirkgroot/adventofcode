739ade2476fc4988921fe4e32740597149bd9ec3
Dirk Groot <dirk+github@dirkgroot.nl>
2025-12-03 09:13:32 +0100

    j_2025_3: Remove unused imports

--- 2025/java/src/test/java/nl/dirkgroot/adventofcode/year2025/Day03.java
+++ 2025/java/src/test/java/nl/dirkgroot/adventofcode/year2025/Day03.java
@@ -4,10 +4,7 @@ import nl.dirkgroot.adventofcode.util.AoCTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
